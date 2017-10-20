efa84602ad740a40140d2ce2c717493776670740
Revo <revo@MBPs-MacBook-Pro.local>
2017-10-20 12:37:16 +0500

    sonarqube stage

--- Jenkinsfile
+++ Jenkinsfile
@@ -20,7 +20,14 @@ pipeline {
                   reportName: "JaCoCo Report"
 				])
              	sh "./gradlew jacocoTestCoverageVerification"
-        		}
-     	}
+        			}
+     		}
+     		stage('SonarQube analysis') {
+    					withSonarQubeEnv('My SonarQube Server') {
+      				// requires SonarQube Scanner for Gradle 2.1+
+      				// It's important to add --info because of SONARJNKNS-281
+      				sh './gradlew --info sonarqube'
+    				}
+  			}
     	}
 }
--- src/main/java/com/example/demo/CalculatorApplication.java
+++ src/main/java/com/example/demo/CalculatorApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CalculatorApplication {
+//@java.lang.SuppressWarnings  //("squid:S2078")
+public class CalculatorApplication {   //NOSONAR
 
 	public static void main(String[] args) {
-		SpringApplication.run(CalculatorApplication.class, args);
+		
+		SpringApplication.run(CalculatorApplication.class, args); //NOSONAR
 	}
 }
